308c0b090ce65f92cdf1aabc64a63873da7c0ef8
Satjeet Singh <satjeet2005@gmail.com>
2025-06-28 20:53:58 +0530

    add day 6 post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,31 +1,13 @@
 var store = [{
-        "title": "Day 1 - Summer Training",
-        "excerpt":"Today I learned about progress bars and how to simulate them using HTML &amp; CSS. Here’s a quick summary and what I built.  ","categories": ["training"],
-        "tags": ["html","css","blog"],
-        "url": "/training/2025/06/01/day-1.html",
-        "teaser": null
-      },{
-        "title": "Day 2 - Summer Training",
-        "excerpt":"Today I learned about progress bars and how to simulate them using HTML &amp; CSS. Here’s a quick summary and what I built.  ","categories": ["training"],
-        "tags": ["html","css","blog"],
-        "url": "/training/2025/06/02/day-2.html",
-        "teaser": null
-      },{
-        "title": "Day 3 - Summer Training",
-        "excerpt":"Today I learned about progress bars and how to simulate them using HTML &amp; CSS. Here’s a quick summary and what I built.  ","categories": ["training"],
-        "tags": ["html","css","blog"],
-        "url": "/training/2025/06/03/day-3.html",
-        "teaser": null
-      },{
-        "title": "Day 4 - Summer Training",
-        "excerpt":"Today I learned about progress bars and how to simulate them using HTML &amp; CSS. Here’s a quick summary and what I built.  ","categories": ["training"],
-        "tags": ["html","css","blog"],
-        "url": "/training/2025/06/04/day-4.html",
-        "teaser": null
-      },{
         "title": "Day 5 of Summer Training",
         "excerpt":"Day 5 of Summer Training 🚀 Welcome to Day 5 of our summer training program! Today’s session was all about refining our front-end development skills and understanding how the browser renders elements. 🔖 What I Learned Today Today I learned about open source contribution how pull requests work, what is...","categories": [],
         "tags": ["HTML","CSS","Web Development"],
         "url": "/2025/06/27/day-5.html",
+        "teaser": null
+      },{
+        "title": "Day 6 of Summer Training",
+        "excerpt":"🗓️ Day 6 Of Summer Training Today was the 6th day of my summer training….. 💡 What I Learned Today Today I learned how to set up a theme with jekyll and push it to github pages. It was a process full of errors. I spend a large chunk of...","categories": [],
+        "tags": ["HTML","CSS","Web Development","JavaScript"],
+        "url": "/2025/06/28/day-6.html",
         "teaser": null
       }]
