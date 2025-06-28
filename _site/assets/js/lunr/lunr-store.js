24807d8eb1522d3b4280d651cb746a1da7796868
Satjeet Singh <satjeet2005@gmail.com>
2025-06-28 16:23:29 +0530

    New Post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Day 1 - Summer Training",
+        "excerpt":"Today I learned about progress bars and how to simulate them using HTML &amp; CSS. Here’s a quick summary and what I built.  ","categories": ["training"],
+        "tags": ["html","css","blog"],
+        "url": "/training/2025/06/01/day-1.html",
+        "teaser": null
+      },{
+        "title": "Day 2 - Summer Training",
+        "excerpt":"Today I learned about progress bars and how to simulate them using HTML &amp; CSS. Here’s a quick summary and what I built.  ","categories": ["training"],
+        "tags": ["html","css","blog"],
+        "url": "/training/2025/06/02/day-2.html",
+        "teaser": null
+      },{
+        "title": "Day 3 - Summer Training",
+        "excerpt":"Today I learned about progress bars and how to simulate them using HTML &amp; CSS. Here’s a quick summary and what I built.  ","categories": ["training"],
+        "tags": ["html","css","blog"],
+        "url": "/training/2025/06/03/day-3.html",
+        "teaser": null
+      },{
+        "title": "Day 4 - Summer Training",
+        "excerpt":"Today I learned about progress bars and how to simulate them using HTML &amp; CSS. Here’s a quick summary and what I built.  ","categories": ["training"],
+        "tags": ["html","css","blog"],
+        "url": "/training/2025/06/04/day-4.html",
+        "teaser": null
+      },{
+        "title": "Day 5 of Summer Training",
+        "excerpt":"Day 5 of Summer Training 🚀 Welcome to Day 5 of our summer training program! Today’s session was all about refining our front-end development skills and understanding how the browser renders elements. 🔖 What I Learned Today Today I learned about open source contribution how pull requests work, what is...","categories": [],
+        "tags": ["HTML","CSS","Web Development"],
+        "url": "/2025/06/27/day-5.html",
+        "teaser": null
+      }]
